b34146ca1b2a8fcb40a928d938fb020591c994c0
Gabriel Weyer <gabrielweyer@gmail.com>
2022-12-26 12:03:47 +1100

    Remove obsolete Nuke attribute
    [skip-release]

--- build/Build.cs
+++ build/Build.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
@@ -13,7 +12,6 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-[CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
